84175fdbc5ea2ea337fcc357b2a5cc89c6871a81
Yolla Ifliandry <ifliandry11@gmail.com>
2020-06-18 09:06:13 +0700

    Selesai fundamental python

---
+++ fundamental1-konstruksi-dasar.py
@@ -0,0 +1,36 @@
+# KONSTRUKSI DASAR PYTHON
+# SEQUENTIAL: Eksekusi berurutan
+'''
+print('Hello World!')
+print('by yo14')
+print('Tanggal 18 Juni 2020')
+print('-' * 20)
+'''
+
+# PERCABANGAN: Eksekusi Terpilih
+'''
+ingin_cepat = False
+
+if ingin_cepat:
+    print('jalan lurus aja ya!')
+else:
+    print('jalan lain')
+'''
+
+# PERULANGAN
+jumlah_anak = 4
+
+for index_anak in range(1, jumlah_anak+1):
+    print(f'Halo anak #{index_anak}')
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ fundamental2-tipe-data-penting.py
@@ -0,0 +1,34 @@
+
+print('\ntipe data skalar atau sederhana:')
+anak1 = 'Eko'
+anak2 = 'Dwi'
+anak3 = 'Tri'
+anak4 = 'Catur'
+
+print(anak1)
+print(anak2)
+print(anak3)
+print(anak4)
+
+print('\ntipe data list/daftar/array :')
+anak = ['Eko', 'Dwi', 'Tri', 'Catur']
+print(anak)
+anak.append('Limo')
+print(anak)
+
+print('\nSapa anak ke-2 :')
+print(f'Hai {anak[1]}')
+
+print('\nSapa semua anak :')
+for a in anak:
+    print(f'Hai {a}!')
+
+print('\nSapa semua anak: cara ribet')
+for a in range(0, len(anak)):
+    print(f'{a+1}. Hai {anak[a]}')
+
+
+
+
+
+
